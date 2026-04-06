b20de4584eafaa59f4acd2090db575d7799da16b
Afridi Mallick <mallickafridi.work@gmail.com>
2026-04-06 15:23:51 +0530

    Learning - Auth, RWTs, Authorization

--- src/components/FakeAPI.jsx
+++ src/components/FakeAPI.jsx
@@ -1,11 +1,51 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
+import exifr from 'exifr'
 
 const FakeAPI = () => {
-    return (
-        <div class="fakeApi">
+    const [accessToken, setAccessToken] = useState()
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
 
-        </div>
+    const fetchMe = async () => {
+        try {
+            const response = await fetch('https://dummyjson.com/auth/login', {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({
+                    username, // un:emilys 
+                    password, // pw: emilyspass,
+                    expiresInMins: 30, // optional, defaults to 60
+                }),
+            })
+            const data = await response.json()
+            setAccessToken(data.accessToken)
+        }
+        catch (error) {
+            console.error(error)
+        }
+    }
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        fetchMe();
+    }
+
+    return (
+        <div className="fakeAPI">
+            <form className='sign-in-box' onSubmit={handleSubmit}>
+                <input className="input my-2 py-2"
+                    type="text"
+                    placeholder='username'
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)} />
+                <input className="input my-2 py-2"
+                    type="text"
+                    placeholder='password'
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)} />
+                <button className="submit-button" type='submit'> Sign In </button>
+            </form>
+        </div >
     )
 }
-
 export default FakeAPI
